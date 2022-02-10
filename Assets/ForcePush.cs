6d7f892964c01e3148f412274473b0115560f4ad
kc3753 <47250615+kc3753@users.noreply.github.com>
2022-02-09 23:18:31 -0500

    color toggle

--- Assets/ForcePush.cs
+++ Assets/ForcePush.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ForcePush : MonoBehaviour
 {   
     private Rigidbody2D ball;
+    bool isHitted = false;
+    Color defaultcolor;
+    private SpriteRenderer ballcolor;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         ball = gameObject.GetComponent<Rigidbody2D>();
+        ballcolor = GetComponent<SpriteRenderer>();
         ball.AddForce(transform.up * 4, ForceMode2D.Impulse);
         ball.AddForce(transform.right * 4, ForceMode2D.Impulse);
+        defaultcolor = ballcolor.color;
     }
 
     // Update is called once per frame
@@ -27,6 +32,19 @@ public class ForcePush : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other){
         if(other.collider.CompareTag("Player")){
             GameManager.score += 1;
+            if(GameManager.toggle1){
+                if (!isHitted){
+                    isHitted = true;
+                    StartCoroutine("SwitchColor");
+                }
+            }
         }
+    }
+    IEnumerator SwitchColor()
+    {
+        ballcolor.color = new Color(255f, 0f, 0f);
+        yield return new WaitForSeconds(.15f);
+        ballcolor.color = defaultcolor;
+        isHitted = false;
     }
 }
